f82b359419a9a237e9248bd9b904d7526ff24097
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-11-05 22:38:40 -0500

    Updates

--- src/components/TableWrapper/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function TableWrapper(props) {
-  return <div className="table-wrapper">{props.children}</div>;
-}
-
-export default TableWrapper;
--- src/components/Title/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Title(props) {
-  return <h1 className="title text-white">{props.children}</h1>;
-}
-
-export default Title;
